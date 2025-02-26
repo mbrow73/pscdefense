d264d4ffb6b056d57a6916cd55074e6b5c12a0c0
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-26 10:41:52 -0700

    adding all code. Wish me luck lmao

--- modules/psc_endpoint/outputs.tf
+++ modules/psc_endpoint/outputs.tf
@@ -1,4 +1,4 @@
 output "psc_forwarding_rule_ip" {
-  value = google_compute_forwarding_rule.psc_forwarding_rule.ip_address
+  value = google_compute_global_forwarding_rule.psc_forwarding_rule.ip_address
 }
 
