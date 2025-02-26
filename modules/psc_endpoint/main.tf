fe9f26df4e827110a17f66144c50d2c25c62f178
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-26 11:42:45 -0700

    adding all code. Wish me luck lmao

--- modules/psc_endpoint/main.tf
+++ modules/psc_endpoint/main.tf
@@ -8,7 +8,7 @@ resource "google_compute_global_address" "psc_vip" {
   purpose       = "PRIVATE_SERVICE_CONNECT"
   address_type  = "INTERNAL"
   network       = var.network
-  address       = "10.10.0.10"
+  address       = "10.10.0.10/32"
 }
 
 # PSC Forwarding Rule
@@ -16,7 +16,7 @@ resource "google_compute_global_forwarding_rule" "psc_forwarding_rule" {
   name                  = var.psc_name
   project               = var.project_id
   load_balancing_scheme = ""  # Must be empty for direct PSC
-  target                = "storage.googleapis.com"
+  target                = "vpc-sc"
   network               = var.network
   ip_address            = google_compute_global_address.psc_vip.address
   port_range            = "443"
