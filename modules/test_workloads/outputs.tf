1657cc7bdc8b48445988e59520b761626463ea0b
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-25 19:58:52 -0700

    adding all code. Wish me luck lmao

--- modules/test_workloads/outputs.tf
+++ modules/test_workloads/outputs.tf
@@ -2,6 +2,3 @@ output "internal_client_ip" {
   value = google_compute_instance.internal_client.network_interface[0].access_config[0].nat_ip
 }
 
-output "onprem_client_ip" {
-  value = google_compute_instance.onprem_client[0].network_interface[0].access_config[0].nat_ip
-}
