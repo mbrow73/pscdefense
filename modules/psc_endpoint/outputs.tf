a5739f2178a10306202a1860e8483c6569b4bcf9
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-26 10:17:19 -0700

    adding all code. Wish me luck lmao

--- modules/psc_endpoint/outputs.tf
+++ modules/psc_endpoint/outputs.tf
@@ -2,6 +2,3 @@ output "psc_forwarding_rule_ip" {
   value = google_compute_forwarding_rule.psc_forwarding_rule.ip_address
 }
 
-output "psc_neg_self_link" {
-  value = google_compute_region_network_endpoint_group.psc_neg.self_link
-}
