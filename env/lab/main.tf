e0481982f628c698dc976318eed53935b833652f
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-25 20:39:32 -0700

    adding all code. Wish me luck lmao

--- env/lab/main.tf
+++ env/lab/main.tf
@@ -13,11 +13,6 @@ terraform {
   }
 }
 
-resource "google_project_iam_member" "service_account_project_creator" {
-  project = var.controllerproject
-  role    = "roles/resourcemanager.projectCreator"
-  member  = "serviceAccount:terraform@testautomation-451116.iam.gserviceaccount.com"
-}
 
 # Call the network module.
 module "network" {
