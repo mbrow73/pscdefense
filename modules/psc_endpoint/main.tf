f1c1210f4acd1c4e4c5458978162dc54650bdbc5
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-26 11:06:32 -0700

    adding all code. Wish me luck lmao

--- modules/psc_endpoint/main.tf
+++ modules/psc_endpoint/main.tf
@@ -8,6 +8,7 @@ resource "google_compute_global_address" "psc_vip" {
   purpose       = "PRIVATE_SERVICE_CONNECT"
   address_type  = "INTERNAL"
   network       = var.network
+  address       = "10.10.0.10/32"
 }
 
 # PSC Forwarding Rule
