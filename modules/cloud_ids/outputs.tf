9297786de1a55722128d3d2ba20a878fa168d551
mbrow73 <fatsaltytryhard@gmail.com>
2025-02-26 10:31:05 -0700

    adding all code. Wish me luck lmao

--- modules/cloud_ids/outputs.tf
+++ modules/cloud_ids/outputs.tf
@@ -1,4 +1,5 @@
-output "cloud_ids_endpoint_id" {
-  description = "ID of the deployed Cloud IDS endpoint."
-  value       = google_cloud_ids_endpoint.cloud_ids.id
-}
+#output "cloud_ids_endpoint_id" {
+#  description = "ID of the deployed Cloud IDS endpoint."
+#  value       = google_cloud_ids_endpoint.cloud_ids.id
+#}
+#
