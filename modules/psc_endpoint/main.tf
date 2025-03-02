df5daae21f3e1ca5f3d83acc70581e02e4d2de28
mbrow73 <fatsaltytryhard@gmail.com>
2025-03-01 20:50:24 -0700

    adding all code. Wish me luck lmao

--- modules/psc_endpoint/main.tf
+++ modules/psc_endpoint/main.tf
@@ -10,8 +10,18 @@ resource "google_compute_global_address" "psc_vip" {
   network       = var.network
   address       = "10.2.2.1"
 }
+# reserve a private internal address for published service psc-endpoint.
+resource "google_compute_address" "psc_endpoint" {
+  project       = var.project_id
+  name          = "psc-endpoint"
+  purpose       = "PRIVATE_SERVICE_CONNECT"
+  address_type  = "INTERNAL"
+  network       = var.network
+  address       = "10.2.2.2"
+  region        = "us-central1"
+}
 
-# PSC Forwarding Rule
+# PSC Forwarding Rule APIs
 resource "google_compute_global_forwarding_rule" "psc_forwarding_rule" {
   name                  = "googleapispsc"
   project               = var.project_id
@@ -21,3 +31,19 @@ resource "google_compute_global_forwarding_rule" "psc_forwarding_rule" {
   ip_address            = google_compute_global_address.psc_vip.address
 }
 
+resource "google_compute_forwarding_rule" "psc_forwarding_rule" {
+  project               = var.project_id
+  name                  = "psc-forwarding-rule"
+  region                = "us-central1"
+  load_balancing_scheme = "INTERNAL"
+  backend_service       = google_compute_backend_bucket.storage_backend.self_link
+  network               = var.network
+  subnetwork            = "psc-subnet"
+}
+
+resource "google_compute_backend_bucket" "storage_backend" {
+  name        = "storage-backend"
+  bucket_name = "storage-backend"
+  enable_cdn  = false
+  project     = var.project_id
+}
