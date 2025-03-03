bbf4d92a3d7102b580818499d70935e7542a47de
mbrow73 <fatsaltytryhard@gmail.com>
2025-03-02 20:48:55 -0700

    pub service provider setup

--- env/lab/main.tf
+++ env/lab/main.tf
@@ -15,54 +15,54 @@ terraform {
 
 
 # Call the network module.
-module "network" {
-  source          = "../../modules/network"
-  project_name    = var.project_name
-  project_id      = var.project_id
-  org_id          = var.org_id
-  billing_account = var.billing_account
-  network_name    = var.network_name
-  subnet_name     = var.subnet_name
-  subnet_cidr     = var.subnet_cidr
-  region          = var.region
-}
-
-# Call the PSC endpoint module.
-module "psc_endpoint" {
-  source       = "../../modules/psc_endpoint"
-  project_id   = module.network.lab_project_id
-  network      = module.network.network_self_link
-  subnetwork   = module.network.subnet_self_link
-  region       = var.region
-  psc_name     = var.psc_name
-  backend_service_id = module.test_workloads.backend_service_id
-}
-
-# Call the Cloud Firewall Plus module.
-module "cloud_firewall_plus" {
-  source               = "../../modules/cloud_firewall_plus"
-  project_id           = module.network.lab_project_id
-  firewall_policy_name = var.firewall_policy_name
-  psc_port             = var.psc_port
-  target_resource      = var.target_resource
-}
-
-
-
-
-
- #Deploy test workloads.
-module "test_workloads" {
-  source          = "../../modules/test_workloads"
-  ca_cert_pem     = module.cloud_firewall_plus.ca_cert_pem
-  project_id      = module.network.lab_project_id
-  zone            = var.zone
-  network         = module.network.network_self_link
-  subnetwork      = module.network.subnet_self_link
-  onprem_network  = var.onprem_network
-  onprem_subnetwork = var.onprem_subnetwork
-  psc_endpoint_ip = module.psc_endpoint.psc_forwarding_rule_ip
-}
+#module "network" {
+#  source          = "../../modules/network"
+#  project_name    = var.project_name
+#  project_id      = var.project_id
+#  org_id          = var.org_id
+#  billing_account = var.billing_account
+#  network_name    = var.network_name
+#  subnet_name     = var.subnet_name
+#  subnet_cidr     = var.subnet_cidr
+#  region          = var.region
+#}
+#
+## Call the PSC endpoint module.
+##module "psc_endpoint" {
+#  source       = "../../modules/psc_endpoint"
+#  project_id   = module.network.lab_project_id
+#  network      = module.network.network_self_link
+#  subnetwork   = module.network.subnet_self_link
+#  region       = var.region
+#  psc_name     = var.psc_name
+#  backend_service_id = module.test_workloads.backend_service_id
+#}
+#
+## Call the Cloud Firewall Plus module.
+#module "cloud_firewall_plus" {
+#  source               = "../../modules/cloud_firewall_plus"
+#  project_id           = module.network.lab_project_id
+#  firewall_policy_name = var.firewall_policy_name
+#  psc_port             = var.psc_port
+#  target_resource      = var.target_resource
+#}
+#
+#
+#
+#
+#
+# #Deploy test workloads.
+#module "test_workloads" {
+#  source          = "../../modules/test_workloads"
+#  ca_cert_pem     = module.cloud_firewall_plus.ca_cert_pem
+#  project_id      = module.network.lab_project_id
+#  zone            = var.zone
+#  network         = module.network.network_self_link
+#  subnetwork      = module.network.subnet_self_link
+#  onprem_network  = var.onprem_network
+#  onprem_subnetwork = var.onprem_subnetwork
+#  psc_endpoint_ip = module.psc_endpoint.psc_forwarding_rule_ip
+#}
 
 #module "cloud_ids" {
 #  subnetwork_name = "lab-subnet"
