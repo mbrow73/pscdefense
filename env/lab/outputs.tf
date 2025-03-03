4a788e6f5502888c9a239799de138b20098d8158
mbrow73 <fatsaltytryhard@gmail.com>
2025-03-02 20:49:33 -0700

    pub service provider setup

--- env/lab/outputs.tf
+++ env/lab/outputs.tf
@@ -1,7 +1,7 @@
-output "lab_project_id" {
-  value = module.network.lab_project_id
-}
-
-output "psc_endpoint_ip" {
-  value = module.psc_endpoint.psc_forwarding_rule_ip
-}
+#output "lab_project_id" {
+#  value = module.network.lab_project_id
+#}
+#
+#output "psc_endpoint_ip" {
+#  value = module.psc_endpoint.psc_forwarding_rule_ip
+#}
